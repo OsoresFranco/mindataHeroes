06d8fa7a923c416991d8bb3fcb28c1f54249a41c
Franco Osores <franco_osores@hotmail.com>
2024-11-21 14:22:16 -0300

    Test: Deleted not tested components and added test to service and dashboard

--- src/app/features/services/hero.service.spec.ts
+++ src/app/features/services/hero.service.spec.ts
@@ -1,16 +1,113 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { HeroService } from './hero.service';
+import { Hero } from '../models/HeroI.interface';
+import { HeroResponseI } from '../models/HeroResponseI.interface';
+import { of } from 'rxjs';
 
 describe('HeroService', () => {
   let service: HeroService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HeroService],
+    });
     service = TestBed.inject(HeroService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve all heroes with pagination and filter by name', () => {
+    const mockHeroResponse: HeroResponseI = {
+      itemsTotal: 2,
+      items: [
+        { id: 1, name: 'Hero 1', bio: 'Bio 1', images: [] },
+        { id: 2, name: 'Hero 2', bio: 'Bio 2', images: [] },
+      ],
+    };
+    service.getAllHeroes(1, 10, 'Hero').subscribe((response) => {
+      expect(response).toEqual(mockHeroResponse);
+      expect(response.items.length).toBe(2);
+      expect(response.items[0].name).toBe('Hero 1');
+    });
+    const req = httpMock.expectOne(
+      `${service['apiUrl']}?hero_name=Hero&page=1&per_page=10`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockHeroResponse);
+  });
+
+  it('should retrieve a hero by ID', () => {
+    const mockHero: Hero = { id: 1, name: 'Hero 1', bio: 'Bio 1', images: [] };
+
+    service.getHeroById(1).subscribe((response) => {
+      expect(response).toEqual(mockHero);
+      expect(response.id).toBe(1);
+      expect(response.name).toBe('Hero 1');
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockHero);
+  });
+
+  it('should create a new hero', () => {
+    const mockHero: Hero = { id: 1, name: 'Hero 1', bio: 'Bio 1', images: [] };
+
+    service.createHero(mockHero).subscribe((response) => {
+      expect(response).toEqual(mockHero);
+    });
+    const req = httpMock.expectOne(`${service['apiUrl']}`);
+
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockHero);
+
+    req.flush(mockHero);
+  });
+
+  it('should update a hero', () => {
+    const mockHero: Hero = {
+      id: 1,
+      name: 'Hero',
+      bio: 'Bio',
+      images: [],
+    };
+    const updatedHero: Hero = {
+      id: 1,
+      name: 'Updated Hero',
+      bio: 'Updated Bio',
+      images: [],
+    };
+    service.updateHero(1, mockHero).subscribe((response) => {
+      expect(response).toEqual(updatedHero);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}/1`);
+    expect(req.request.method).toBe('PATCH');
+    expect(req.request.body).toEqual(mockHero);
+
+    req.flush(updatedHero);
+  });
+
+  it('should delete a hero', () => {
+    service.deleteHero(1).subscribe((response) => {
+      expect(response).toBeNull();
+    });
+    const req = httpMock.expectOne(`${service['apiUrl']}/1`);
+
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
   });
 });
--- src/app/features/views/dashboard/dashboard.component.spec.ts
+++ src/app/features/views/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,70 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
+
+import { of } from 'rxjs';
+import { SearchBarService } from '../../../core/services/search-bar.service';
+import { HeroService } from '../../services/hero.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let searchBarService: jasmine.SpyObj<SearchBarService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    const searchBarSpy = jasmine.createSpyObj('SearchBarService', [
+      'searchValue$',
+    ]);
+    const heroServiceSpy = jasmine.createSpyObj('HeroService', [
+      'getAllHeroes',
+    ]);
+
+    TestBed.configureTestingModule({
+      imports: [DashboardComponent, HttpClientModule],
+      providers: [
+        { provide: SearchBarService, useValue: searchBarSpy },
+        { provide: HeroService, useValue: heroServiceSpy },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    searchBarService = TestBed.inject(
+      SearchBarService
+    ) as jasmine.SpyObj<SearchBarService>;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should update searchTerm and reset currentPage when searchValue$ emits', () => {
+    const mockSearchTerm = 'Superman';
+    searchBarService.searchValue$ = of(mockSearchTerm);
+    spyOn(component, 'getAllheroes');
+
+    component.getCurrentSearchTerm();
+
+    expect(component.searchTerm).toBe(mockSearchTerm);
+    expect(component.currentPage).toBe(1);
+    expect(component.getAllheroes).toHaveBeenCalled();
+  });
+
+  it('should update currentPage and call getAllheroes', () => {
+    spyOn(component, 'getAllheroes');
+    const newPage = 2;
+    component.onPageChange(newPage);
+    expect(component.currentPage).toBe(newPage);
+    expect(component.getAllheroes).toHaveBeenCalled();
+  });
+
+  it('should update itemsPerPage and call getAllheroes', () => {
+    spyOn(component, 'getAllheroes');
+    const newPerPage = 20;
+    component.onPerPageChange(newPerPage);
+    expect(component.itemsPerPage).toBe(newPerPage);
+    expect(component.getAllheroes).toHaveBeenCalled();
+  });
+
+  it('should update currentViewMode', () => {
+    const newViewMode: 'grid' | 'list' = 'grid';
+    component.onViewModeChange(newViewMode);
+    expect(component.currentViewMode).toBe(newViewMode);
   });
 });
