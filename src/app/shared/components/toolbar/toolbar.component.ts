94784f6f32233cb6e1c0eb314b2a986983362d55
Franco Osores <franco_osores@hotmail.com>
2024-11-21 11:37:00 -0300

    Feat: Added Add Hero button to toolbar

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   imports: [],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
+  constructor(private router: Router) {}
 
+  onAdd() {
+    this.router.navigate(['/forms']);
+  }
 }
